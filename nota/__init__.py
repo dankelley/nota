9455750ff4579c259e29fcbb61777310eb6e4981
Dan Kelley <kelley.dan@gmail.com>
2015-02-26 10:31:38 -0400

    start packaging work

---
+++ nota/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+from main import nota
+if __name__ == "__main__":
+    nota()
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nota',
+      version='0.5',
+      description='Text-based note taker',
+      url='https://github.com/dankelley/nota',
+      author='Dan Kelley',
+      author_email='kelley.dan@google.com',
+      license='GPL3',
+      packages=['nota'],
+      zip_safe=False)
