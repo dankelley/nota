272af1042a1ebe3cab07248e2fc4b0e2d763277d
Dan Kelley <kelley.dan@gmail.com>
2015-09-21 07:09:17 -0300

    version number updated

--- nota/notaclass.py
+++ nota/notaclass.py
@@ -50,7 +50,7 @@ class Nota:
         self.cur = con.cursor()
         self.authorId = authorId
         ## 0.3: add note.modified column
-        self.appversion = [0, 7, 3] # db schema changes always yield first or second digit increment
+        self.appversion = [0, 7, 6] # db schema changes always yield first or second digit increment
         self.dbversion = self.appversion
         if mustInitialize:
             print("Initializing database; run 'nota' again to use it.")
